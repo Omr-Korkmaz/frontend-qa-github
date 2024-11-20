1dbf0ea48f1cbe23b3b4278e6c9b0af52c859486
Omer Korkmaz <h.omr.korkmaz@gmail.com>
2024-11-21 00:53:44 +0100

    fix the bug and make responsive navigation bar with login and logout options

--- src/app/components/navigation-menu/navigation-menu.component.ts
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -1,6 +1,86 @@
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { UserService } from '../../services/user.service';
+// import { GithubUser } from '../../model/github.model';
+// import { TegelModule } from '@scania/tegel-angular-17';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { Subscription } from 'rxjs';
+
+// @Component({
+//   selector: 'app-navigation-menu',
+//   standalone: true,
+//   imports: [TegelModule, FormsModule, CommonModule],
+//   templateUrl: './navigation-menu.component.html',
+//   styleUrls: ['./navigation-menu.component.scss'],
+// })
+// export class NavigationMenuComponent implements OnInit, OnDestroy {
+//   userInfo: GithubUser | null = null;
+//   private subscription: Subscription = new Subscription();
+
+//   constructor(private userService: UserService) {}
+
+//   ngOnInit(): void {
+  
+//     this.subscription.add(
+//       this.userService.userInfo$.subscribe((userInfo) => {
+//         this.userInfo = userInfo; 
+//       })
+//     );
+//   }
+
+//   ngOnDestroy(): void {
+//     this.subscription.unsubscribe();
+//   }
+// }
+
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { UserService } from '../../services/user.service';
+// import { GithubUser } from '../../model/github.model';
+// import { TegelModule } from '@scania/tegel-angular-17';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { Subscription } from 'rxjs';
+
+// @Component({
+//   selector: 'app-navigation-menu',
+//   standalone: true,
+//   imports: [TegelModule, FormsModule, CommonModule],
+//   templateUrl: './navigation-menu.component.html',
+//   styleUrls: ['./navigation-menu.component.scss'],
+// })
+// export class NavigationMenuComponent implements OnInit, OnDestroy {
+//   userInfo: GithubUser | null = null;
+//   private subscription: Subscription = new Subscription();
+//   isMenuOpen = false;
+
+//   constructor(private userService: UserService) {}
+
+//   ngOnInit(): void {
+//     this.subscription.add(
+//       this.userService.userInfo$.subscribe((userInfo) => {
+//         this.userInfo = userInfo;
+//       })
+//     );
+//   }
+
+//   toggleMenu(): void {
+//     this.isMenuOpen = !this.isMenuOpen;
+//   }
+
+//   closeMenu(): void {
+//     this.isMenuOpen = false;
+//   }
+
+//   ngOnDestroy(): void {
+//     this.subscription.unsubscribe();
+//   }
+// }
+
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { GithubUser } from '../../model/github.model';
+import { Router } from '@angular/router';
 import { TegelModule } from '@scania/tegel-angular-17';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -15,20 +95,42 @@ import { Subscription } from 'rxjs';
 })
 export class NavigationMenuComponent implements OnInit, OnDestroy {
   userInfo: GithubUser | null = null;
+  isMenuOpen = false; // Side menu state
   private subscription: Subscription = new Subscription();
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-  
     this.subscription.add(
       this.userService.userInfo$.subscribe((userInfo) => {
-        this.userInfo = userInfo; 
+        this.userInfo = userInfo;
       })
     );
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  logIn(): void {
+    this.router.navigate(['/auth']); // Navigate to the login page
+  }
+
+  logOut(): void {
+    this.userService.clearUserInfo(); // Clear user info
+    this.userInfo = null; // Set the user as logged out
+    this.router.navigate(['/auth']); // Redirect to login page
+  }
+
+  get isAuthenticated(): boolean {
+    return !!this.userInfo; // Check if the user is authenticated
   }
 }
--- src/app/pages/dashboard-page/components/language-chart/language-chart.component.ts
+++ src/app/pages/dashboard-page/components/language-chart/language-chart.component.ts
@@ -1,4 +1,19 @@
-import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+// import { ChartComponent } from '../../../../components/chart/chart.component';
+
+// @Component({
+//   selector: 'app-language-chart',
+//   standalone: true,
+//   imports: [ChartComponent],
+//   templateUrl: './language-chart.component.html',
+//   styleUrls: ['./language-chart.component.scss']
+// })
+// export class LanguageChartComponent {
+//   @Input() chartData: number[] = [];
+//   @Input() chartLabels: string[] = [];
+// }
+
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartComponent } from '../../../../components/chart/chart.component';
 
 @Component({
@@ -8,7 +23,14 @@ import { ChartComponent } from '../../../../components/chart/chart.component';
   templateUrl: './language-chart.component.html',
   styleUrls: ['./language-chart.component.scss']
 })
-export class LanguageChartComponent {
+export class LanguageChartComponent implements OnInit {
   @Input() chartData: number[] = [];
   @Input() chartLabels: string[] = [];
+  chartColors: string[] = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'];
+
+  ngOnInit(): void {
+    if (this.chartData.length !== this.chartLabels.length) {
+      console.error('Mismatch between chart data and labels.');
+    }
+  }
 }
